54319ce9df271bb732b9d7a4800d1c11c09c76df
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-07-02 11:10:53 +0300

    finish css file

---
+++ main.js
@@ -0,0 +1,9 @@
+// const container = document.getElementById("container");
+
+// for (let i = 0; i < circles_no; i++) {
+//   const circle = document.createElement("div");
+//   circle.classList.add("circle");
+//   circle.addEventListener("click", () => {});
+
+//   container.appendChild(circle);
+// }
